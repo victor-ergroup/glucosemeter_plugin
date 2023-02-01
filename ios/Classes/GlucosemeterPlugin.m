517ec19049afee0fd4e6ed6e13b75c4753ea243b
Victor Tan <victor@ergrouptech.com>
2023-02-01 10:13:49 +0800

    import GlucoseMeter Objective-C dependency

--- ios/Classes/GlucosemeterPlugin.m
+++ ios/Classes/GlucosemeterPlugin.m
@@ -1,4 +1,5 @@
 #import "GlucosemeterPlugin.h"
+#import "JTManager+BloodSugar.h"
 
 @implementation GlucosemeterPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
@@ -12,9 +13,15 @@
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
   if ([@"getPlatformVersion" isEqualToString:call.method]) {
     result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
-  } else {
+  } else if ([@"automaticConnectBluetooth" isEqualToString:call.method]){
+      [super automaticConnectBluetooth:call:call result:result];
+  }else {
     result(FlutterMethodNotImplemented);
   }
+}
+
++ (void)automaticConnectBluetooth:(FlutterMethodCall*)call result:(FlutterResult)result {
+    NSLog(@"test");
 }
 
 @end
